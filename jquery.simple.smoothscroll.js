94e15ec948a6514dba4bb185f8dd533c84b3a1fa
simov <ssimo@abv.bg>
2012-10-20 21:24:06 +0300

    testing some stuff

--- jquery.simple.smoothscroll.js
+++ jquery.simple.smoothscroll.js
@@ -5,55 +5,54 @@
  * Licensed under GPLv3.
  */
 (function( $ ) {
-	
-	$.smoothscroll = function(options) {
-	
-	var _ = $.extend({
-		step: 55,
-		speed: 400
-	}, options || {});
-	
-	// smoothscroll logic below
-	var top = 0,
-		step = _.step,
-		speed = _.speed,
-		viewport = $(window).height(),
-		body = $.browser.webkit ? $('body') : $('html'),
-		wheel = false;
+    
+    $.smoothscroll = function(options) {
+    
+    var _ = $.extend({
+        step: 55,
+        speed: 400
+    }, options || {});
+    
+    // smoothscroll logic below
+    var top = 0,
+        step = _.step,
+        speed = _.speed,
+        viewport = $(window).height(),
+        body = $.browser.webkit ? $('body') : $('html'),
+        wheel = false;
 
 
-	$('body').mousewheel(function(event, delta) {
+    $('body').mousewheel(function(event, delta) {
 
-		wheel = true;
+        wheel = true;
 
-		if (delta < 0) {
+        if (delta < 0) {
 
-			top = (top+viewport) >= $(document).height() ? top : top+=step;
+            top = (top+viewport) >= $(document).height() ? top : top+=step;
 
-			body.stop().animate({scrollTop: top}, speed, function () {
-				wheel = false;
-			});
-		} else {
+            body.stop().animate({scrollTop: top}, speed, function () {
+                wheel = false;
+            });
+        } else {
 
-			top = top <= 0 ? 0 : top-=step;
+            top = top <= 0 ? 0 : top-=step;
 
-			body.stop().animate({scrollTop: top}, speed, function () {
-				wheel = false;
-			});
-		}
+            body.stop().animate({scrollTop: top}, speed, function () {
+                wheel = false;
+            });
+        }
 
-		return false;
-	});
+        return false;
+    });
 
-	$(window).on('resize', function (e) {
-		viewport = $(this).height();
-	});
+    $(window).on('resize', function (e) {
+        viewport = $(this).height();
+    });
 
-	$(window).on('scroll', function (e) {
-		if (!wheel)
-			top = $(this).scrollTop();
-	});
-	
-	};
+    $(window).on('scroll', function (e) {
+        if (!wheel)
+            top = $(this).scrollTop();
+    });
+    
+    };
 })( jQuery );
-// test
